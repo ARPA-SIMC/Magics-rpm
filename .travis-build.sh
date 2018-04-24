92c14fcd018160abe8ce51a1567ce7e395626c63
Daniele Branchini <dbranchini@arpa.emr.it>
2018-04-24 10:36:55 +0200

    trying less verbose output

--- .travis-build.sh
+++ .travis-build.sh
@@ -8,25 +8,25 @@ then
     pkgcmd="yum"
     builddep="yum-builddep"
     sed -i '/^tsflags=/d' /etc/yum.conf
-    yum install -y epel-release
-    yum install -y @buildsys-build
-    yum install -y yum-utils
-    yum install -y git
-    yum install -y rpmdevtools
-    yum install -y yum-plugin-copr
-    yum install -y pv
-    yum copr enable -y simc/stable
+    yum install -qy epel-release
+    yum install -qy @buildsys-build
+    yum install -qy yum-utils
+    yum install -qy git
+    yum install -qy rpmdevtools
+    yum install -qy yum-plugin-copr
+    yum install -qy pv
+    yum copr enable -qy simc/stable
 elif [[ $image =~ ^fedora: ]]
 then
     pkgcmd="dnf"
     builddep="dnf builddep"
     sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
-    dnf install -y @buildsys-build
-    dnf install -y 'dnf-command(builddep)'
-    dnf install -y git
-    dnf install -y rpmdevtools
-    dnf install -y pv
-    dnf copr enable -y simc/stable
+    dnf install -qy @buildsys-build
+    dnf install -qy 'dnf-command(builddep)'
+    dnf install -qy git
+    dnf install -qy rpmdevtools
+    dnf install -qy pv
+    dnf copr enable -qy simc/stable
 fi
 
 $builddep -y Magics.spec
@@ -37,6 +37,7 @@ then
     mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
     cp Magics.spec ~/rpmbuild/SPECS/
     spectool -g -R ~/rpmbuild/SPECS/Magics.spec
+    set +x
     rpmbuild -ba ~/rpmbuild/SPECS/Magics.spec 2>&1 | pv -q -L 3k
     find ~/rpmbuild/{RPMS,SRPMS}/ -name "${pkgname}*rpm" -exec cp -v {} . \;
     # TODO upload ${pkgname}*.rpm to github release on deploy stage
